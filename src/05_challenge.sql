5c956c942977716f1170989b425d1eaf896c5c46
Claudio Meira <claudiomeirabs@gmail.com>
2024-06-11 18:42:48 -0300

    Alterando requisito 05

--- src/05_challenge.sql
+++ src/05_challenge.sql
@@ -1,1 +1,1 @@
-SELECT full_name AS 'Nome completo', birthday AS 'Data de nascimento' FROM users WHERE active = 1 AND  YEAR(birthday) >= 1990;
+SELECT full_name AS 'Nome completo', birthday AS 'Data de nascimento' FROM users WHERE active = true AND  YEAR(birthday) >= 1990;
