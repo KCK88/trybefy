df039297c3f99ea997cca0be3704c53d3b17663b
Claudio Meira <claudiomeirabs@gmail.com>
2024-06-12 18:03:49 -0300

    Requisito 29 feito e testato

--- src/28_challenge.sql
+++ src/28_challenge.sql
@@ -1,11 +1,4 @@
-SELECT
-    users.full_name AS "Pessoa usuária",
-    COUNT(artists_followers.artist_id) AS "Artistas que segue"
-FROM
-    users
-LEFT JOIN
-    artists_followers ON users.id = artists_followers.user_id
-GROUP BY
-    users.full_name
-ORDER BY
-    users.full_name ASC;
+SELECT users.full_name AS "Pessoa usuária", COUNT(artists_followers.artist_id) AS "Artistas que segue" FROM users
+LEFT JOIN artists_followers ON users.id = artists_followers.user_id
+GROUP BY users.full_name
+ORDER BY users.full_name ASC;
